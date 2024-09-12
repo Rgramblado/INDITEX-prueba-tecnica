7497db92b303f72c7ccd0c5b06b21d36f069a882
Rafael Gonzalez Ramblado <rgramblado@gmail.com>
2024-09-13 00:18:10 +0200

    PATCH - Fix version manager

--- version_manager.py
+++ version_manager.py
@@ -1,12 +1,13 @@
-import sys
 import subprocess
-import os
 
 def get_latest_tag():
     try:
         return subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).decode().strip()
     except subprocess.CalledProcessError:
         return '0.0.0'
+
+def get_last_commit_message():
+    return subprocess.check_output(['git', 'log', '-1', '--pretty=%B']).decode().strip()
 
 def increment_version(version, increment_type):
     major, minor, patch = map(int, version.split('.'))
@@ -17,37 +18,22 @@ def increment_version(version, increment_type):
     else:  # PATCH
         return f'{major}.{minor}.{patch + 1}'
 
-def is_amend_commit():
-    current_count = int(subprocess.check_output(['git', 'rev-list', 'HEAD', '--count']).decode().strip())
-    
-    if os.path.exists('.temp/.last-commit-count.tmp'):
-        with open('.temp/.last-commit-count.tmp', 'r') as f:
-            previous_count = int(f.read().strip())
-        return current_count == previous_count
-    return False
-
-def create_or_update_tag(tag_name):
-    # Crea el tag o lo actualiza si ya existe
-    subprocess.run(['git', 'tag', '-f', tag_name])
-    # Asegura que el tag esté en el HEAD actual
-    subprocess.run(['git', 'push', '--force', 'origin', tag_name])
+def create_tag(tag_name):
+    subprocess.run(['git', 'tag', tag_name])
+    subprocess.run(['git', 'push', 'origin', tag_name])
 
 def main():
-    commit_msg = sys.argv[1]
-    increment_type = commit_msg.split()[0]
-    
-    if is_amend_commit():
-        print('Commit amend detectado. Se actualizará el tag existente.')
-        latest_tag = get_latest_tag()
-        create_or_update_tag(latest_tag)
-        print(f'Tag actualizado: {latest_tag}')
-    else:
-        latest_tag = get_latest_tag()
-        new_version = increment_version(latest_tag, increment_type)
-        create_or_update_tag(new_version)
-        print(f'Nuevo tag creado: {new_version}')
+    commit_msg = get_last_commit_message()
+    increment_type = commit_msg.split()[0].upper()
 
-    # No es necesario limpiar el archivo temporal aquí, ya que se actualizará en el próximo pre-commit
+    if increment_type not in ['MAJOR', 'MINOR', 'PATCH']:
+        print("No se requiere un nuevo tag.")
+        return
+
+    latest_tag = get_latest_tag()
+    new_version = increment_version(latest_tag, increment_type)
+    create_tag(new_version)
+    print(f'Nuevo tag creado: {new_version}')
 
 if __name__ == '__main__':
     main()
